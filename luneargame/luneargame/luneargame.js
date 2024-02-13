43806b276ab4414d821ac271c7bfb55e82196767
R-cygr <ellasaerayy@gmail.com>
2024-02-13 16:36:02 +0100

    Update luneargame.js

--- luneargame/luneargame/luneargame.js
+++ luneargame/luneargame/luneargame.js
@@ -1,53 +1,32 @@
-let x = 100;
-let y = 100;
-let rotation = 0;
-let speed = 0;
-let lastMouseX = 0;
-let lastMouseY = 0;
-let mouseIsActive = false;
+//MOON
+fill(250,250,250);
+stroke(250,250,250);
+ellipse(270,300,700,170);
 
-function setup() {
-    createCanvas(400, 400);
-}
+fill(211,211,211);
+ellipse(50,300,80,60);
 
-function draw() {
-   background(49, 173, 220);
+fill(211,211,211);
+ellipse(230,250,80,60);
 
-    //  spaceship
-    push();
-    translate(x, y);
-    rotate(rotation);
-   
-    fill(255, 255, 255);
-    ellipse(0, 20, 80, 130);
-    
-    fill(255, 192, 203);
-    ellipse(0, 0, 30, 30);
-    
-    fill(0);
-    ellipse(0, 0, 20, 20);
-    
-    pop();
 
-    x = x + Math.cos(rotation) * speed;
-    y = y + Math.sin(rotation) * speed;
+fill(211,211,211);
+ellipse(400,300,80,60);
 
- 
-    if (keyIsDown(38)) {
-        speed = 5;
-    } else if (keyIsDown(40)) {
-        speed = -5;
-    } else {
-        speed = 0;
-    }
 
-    if (keyIsDown(37)) {
-        rotation = rotation - 0.05;
-    } else if (keyIsDown(39)) {
-        rotation = rotation + 0.05;
-    }
+//SPACESHIP
+fill(250,250,250);
+stroke(0,0,0);
+ellipse(230,200,80,150);
+
+ellipse(230,170,40,40);
+ellipse(230,170,30,30);
 
 
 
-    
-}
+
+
+
+
+
+
